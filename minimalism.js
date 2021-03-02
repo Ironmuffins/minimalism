2080abd5d5f09d5d596375e3041035b62c364698
Jared <midnightfuture@protonmail.com>
2021-03-01 19:58:13 -0800

    Added 'add item' button.

--- minimalism.js
+++ minimalism.js
@@ -1,4 +1,17 @@
+//Initial variables
+let allitems = [];
+
+//Item constructor
 function Item(room, name) {
     this.room = room
     this.name = name
 };
+
+//Add item button
+const additem = document.querySelector('#additembutton')
+additem.addEventListener('click', additemFunc);
+
+//Add item function
+function additemFunc() {
+    
+}
